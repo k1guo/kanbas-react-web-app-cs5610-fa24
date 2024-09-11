17703b98f69378a930d1e567bc9336caa1609477
Keying <KEYING01152000@GMAIL.COM>
2024-09-11 15:46:46 -0700

    update the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Labs from './Labs'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          welcome to Web APP!!!!!!
-          !!!!!!!!!
-
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+    <h1>Welcome to Keying Guo's Web Dev</h1>
+    <Labs />
+   </div>
   );
 }
 
