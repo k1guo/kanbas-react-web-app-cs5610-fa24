0e5f6ba2a84509e6f6c784c3da63aa66c7b8ff5a
Keying <KEYING01152000@GMAIL.COM>
2024-09-14 13:39:56 -0700

    make Kanbas the default screen

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import "./App.css";
 import Labs from "./Labs";
 import Kanbas from "./Kanbas";
-import { HashRouter, Link, Route, Routes } from "react-router-dom";
+import { HashRouter, Link, Route, Routes ,Navigate} from "react-router-dom";
 
+// landing page
 function App() {
   return (
       <HashRouter>
         <div>
           <Routes>
             {/* switch case statement */}
+            <Route path="/" element={<Navigate to="/Kanbas" />} />
             <Route path="/Labs/*" element={<Labs />} />
             <Route path="/Kanbas/*" element={<Kanbas />} />
           </Routes>
