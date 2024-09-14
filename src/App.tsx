67b9c67b9b31d99564345e39a603d30f7d10f0a7
Keying <KEYING01152000@GMAIL.COM>
2024-09-14 13:30:54 -0700

    lecture2 Lab part

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import './App.css';
-import Labs from './Labs'
+import React from "react";
+import "./App.css";
+import Labs from "./Labs";
+import Kanbas from "./Kanbas";
+import { HashRouter, Link, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-   <div>
-    <h1>Welcome to Keying Guo's Web Dev</h1>
-    <Labs />
-   </div>
+      <HashRouter>
+        <div>
+          <Routes>
+            {/* switch case statement */}
+            <Route path="/Labs/*" element={<Labs />} />
+            <Route path="/Kanbas/*" element={<Kanbas />} />
+          </Routes>
+        </div>
+      </HashRouter>
   );
 }
 
