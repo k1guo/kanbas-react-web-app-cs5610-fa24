afddf1e34fa3445ad40579199506b3e2a83c49e0
Keying <KEYING01152000@GMAIL.COM>
2024-09-29 21:50:37 -0700

    update github info

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -36,7 +36,7 @@ export default function TOC() {
         </a>
       </li>
       <li className="nav-item">
-        <a id="wd-k" href="https://github.com/jannunzi" className="nav-link">
+        <a id="wd-k" href="https://github.com/k1guo" className="nav-link">
           My GitHub
         </a>
       </li>
